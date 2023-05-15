1a32e34f7c7dad969600136799ff26227bb03605
James Jacobsen <edjcjacobsen@gmail.com>
2023-05-15 14:16:40 -0500

    added KitchenForm and called it in about page for testing purposes

---
+++ src/components/Client/KitchenForm.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import {useSelector} from 'react-redux';
+import {TextField, FormControl} from '@mui/material';
+
+function KitchenForm(){
+    return (
+        <>
+            
+        </>
+    );
+}
+
+export default KitchenForm;
