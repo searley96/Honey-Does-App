6a952d58cfcfe9aae4a6a15a44f400c6f4526520
Fue Lee <fueclee.12@gmail.com>
2023-05-15 16:52:10 -0500

    added functionality to the edit button on each information field

--- src/components/ProfilePage/ProfilePage.jsx
+++ src/components/ProfilePage/ProfilePage.jsx
@@ -1,17 +1,52 @@
-import { Box, Button, Card, Typography } from '@mui/material';
+import { Box, Button, Card, IconButton, Stack, TextField, Typography } from '@mui/material';
+
+import CloseIcon from '@mui/icons-material/Close';
+import CheckIcon from '@mui/icons-material/Check';
+
+import { useState } from 'react';
+import { useSelector } from 'react-redux';
 
 
 function ProfilePage () {
+
+    const user = useSelector(store => store.user);
 
     const cardStyle = {
         display: 'flex',
         justifyContent: 'space-between',
         alignItems: 'center',
         width: '90%',
-        paddingY: .5,
+        paddingY: 1,
         my: 1,
-        backgroundColor: 'whitesmoke'
+        backgroundColor: 'whitesmoke',
+        maxHeight: '52.5px'
     }
+
+    const addressCardStyle = {
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        width: '90%',
+        paddingY: 1,
+        my: 1,
+        backgroundColor: 'whitesmoke',
+        height: '100px'
+    }
+
+    // edit information switches
+    const [editFirstNameBtn, setEditFirstNameBtn] = useState(false);
+    const [editLastNameBtn, setEditLastNameBtn] = useState(false);
+    const [editEmailBtn, setEditEmailBtn] = useState(false);
+    const [editPasswordBtn, setEditPasswordBtn] = useState(false);
+    const [editPhoneNumberBtn, setEditPhoneNumberBtn] = useState(false);
+    const [editAddressBtn, setEditAddressBtn] = useState(false);
+
+    const [editFirstNameValue, setEditFirstNameValue] = useState();
+    const [editLastNameValue, setEditLastNameValue] = useState();
+    const [editEmailValue, setEmailValue] = useState('');
+    const [editPasswordValue, setPasswordValue] = useState();
+    const [editPhoneNumberValue, setEditPhoneNumberValue] = useState();
+    const [editAddressValue, setEditAddressValue] = useState();
 
     return(
         <>
@@ -27,43 +62,142 @@ function ProfilePage () {
                     First Name:
                 </Typography>
                 <Card sx={cardStyle}>
-                    <Typography sx={{display: 'inline-flex', mx: 2.5}}>First Name</Typography>
-                    <Button variant='outlined' sx={{display: 'inline-flex', mr: 1.5}}>Edit</Button>
+                    {editFirstNameBtn ?
+                        <>
+                            <TextField variant='standard' size='small' value={user.first_name} sx={{display: 'inline-flex', mx: 2.5}}/>
+                            <Box sx={{display: 'inline-flex', mr: 1.5}}>
+                                <Button onClick={() => setEditFirstNameBtn(false)} sx={{border: 'solid 1px red', mr: 1, px: 0}}>
+                                    <CloseIcon sx={{color: 'red'}}/>
+                                </Button>
+                                <Button sx={{border: 'solid 1px green'}}>
+                                    <CheckIcon sx={{color: 'green'}}/>
+                                </Button>
+                            </Box>   
+                        </>
+                        :
+                        <>
+                            <Typography sx={{display: 'inline-flex', mx: 2.5}}>{user.first_name}</Typography>
+                            <Button variant='outlined' onClick={() => setEditFirstNameBtn(true)} sx={{display: 'inline-flex', mr: 1.5}}>Edit</Button>
+                        </>
+                    }
                 </Card>
                 <Typography align='center' variant='h7'>
                     Last Name:
                 </Typography>
                 <Card sx={cardStyle}>
-                    <Typography sx={{display: 'inline-flex', mx: 2.5}}>Last Name</Typography>
-                    <Button variant='outlined' sx={{display: 'inline-flex', mr: 1.5}}>Edit</Button>
+                    {editLastNameBtn ?
+                    <>
+                        <TextField variant='standard' size='small' value={user.last_name} sx={{display: 'inline-flex', mx: 2.5}}/>
+                        <Box sx={{display: 'inline-flex', mr: 1.5}}>
+                            <Button onClick={() => setEditLastNameBtn(false)} sx={{border: 'solid 1px red', mr: 1, px: 0}}>
+                                <CloseIcon sx={{color: 'red'}}/>
+                            </Button>
+                            <Button sx={{border: 'solid 1px green'}}>
+                                <CheckIcon sx={{color: 'green'}}/>
+                            </Button>
+                        </Box>
+                    </>
+                    :
+                    <>
+                        <Typography sx={{display: 'inline-flex', mx: 2.5}}>{user.last_name}</Typography>
+                        <Button variant='outlined' onClick={() => setEditLastNameBtn(true)} sx={{display: 'inline-flex', mr: 1.5}}>Edit</Button>
+                    </>
+                    }
                 </Card>
                 <Typography align='center' variant='h7'>
                     Email:
                 </Typography>
                 <Card sx={cardStyle}>
-                    <Typography sx={{display: 'inline-flex', mx: 2.5}}>User Email Text</Typography>
-                    <Button variant='outlined' sx={{display: 'inline-flex', mr: 1.5}}>Edit</Button>
+                    {editEmailBtn ? 
+                    <>
+                        <TextField variant='standard' size='small' value={user.username} sx={{display: 'inline-flex', mx: 2.5}}/>
+                        <Box sx={{display: 'inline-flex', mr: 1.5}}>
+                            <Button onClick={() => setEditEmailBtn(false)} sx={{border: 'solid 1px red', mr: 1, px: 0}}>
+                                <CloseIcon sx={{color: 'red'}}/>
+                            </Button>
+                            <Button sx={{border: 'solid 1px green'}}>
+                                <CheckIcon sx={{color: 'green'}}/>
+                            </Button>
+                        </Box>
+                    </>
+                    :
+                    <>
+                        <Typography sx={{display: 'inline-flex', mx: 2.5}}>{user.username}</Typography>
+                        <Button variant='outlined' onClick={() => setEditEmailBtn(true)} sx={{display: 'inline-flex', mr: 1.5}}>Edit</Button>
+                    </>
+                    }
                 </Card>
                 <Typography align='center' variant='h7'>
                     Password:
                 </Typography>
                 <Card sx={cardStyle}>
-                    <Typography sx={{display: 'inline-flex', mx: 2.5}}>User Password Text</Typography>
-                    <Button variant='outlined' sx={{display: 'inline-flex', mr: 1.5}}>Edit</Button>
+                    {editPasswordBtn ?
+                    <>
+                        <TextField variant='standard' size='small' value={editPasswordValue} sx={{display: 'inline-flex', mx: 2.5}}/>
+                        <Box sx={{display: 'inline-flex', mr: 1.5}}>
+                            <Button onClick={() => setEditPasswordBtn(false)} sx={{border: 'solid 1px red', mr: 1, px: 0}}>
+                                <CloseIcon sx={{color: 'red'}}/>
+                            </Button>
+                            <Button sx={{border: 'solid 1px green'}}>
+                                <CheckIcon sx={{color: 'green'}}/>
+                            </Button>
+                        </Box>
+                    </> 
+                    :
+                    <>
+                        <Typography sx={{display: 'inline-flex', mx: 2.5}}>User Password Text</Typography>
+                        <Button variant='outlined' onClick={() => setEditPasswordBtn(true)} sx={{display: 'inline-flex', mr: 1.5}}>Edit</Button>
+                    </>
+                    }
                 </Card>
                 <Typography align='center' variant='h7'>
                     Phone Number:
                 </Typography>
                 <Card sx={cardStyle}>
-                    <Typography sx={{display: 'inline-flex', mx: 2.5}}>111-111-1111</Typography>
-                    <Button variant='outlined' sx={{display: 'inline-flex', mr: 1.5}}>Edit</Button>
+                    {editPhoneNumberBtn ?
+                    <>
+                        <TextField variant='standard' size='small' value={user.phone_number} sx={{display: 'inline-flex', mx: 2.5}}/>
+                        <Box sx={{display: 'inline-flex', mr: 1.5}}>
+                            <Button onClick={() => setEditPhoneNumberBtn(false)} sx={{border: 'solid 1px red', mr: 1, px: 0}}>
+                                <CloseIcon sx={{color: 'red'}}/>
+                            </Button>
+                            <Button sx={{border: 'solid 1px green'}}>
+                                <CheckIcon sx={{color: 'green'}}/>
+                            </Button>
+                        </Box>
+                    </> 
+                    :
+                    <>
+                        <Typography sx={{display: 'inline-flex', mx: 2.5}}>{user.phone_number}</Typography>
+                        <Button variant='outlined' onClick={() => setEditPhoneNumberBtn(true)} sx={{display: 'inline-flex', mr: 1.5}}>Edit</Button>
+                    </>
+                    }
                 </Card>
                 <Typography align='center' variant='h7'>
                     Address:
                 </Typography>
-                <Card sx={cardStyle}>
-                    <Typography sx={{display: 'inline-flex', mx: 2.5}}>1234 Fake Street Fake City, Fake State Fake Country 12345</Typography>
-                    <Button variant='outlined' sx={{display: 'inline-flex', mr: 1.5}}>Edit</Button>
+                <Card sx={addressCardStyle}>
+                    {editAddressBtn ?
+                    <>
+                        <TextField multiline variant='standard' size='small' value={user.address} sx={{display: 'inline-flex', mx: 2.5, width: 250}}/>
+                        <Box sx={{display: 'inline-flex', mr: 1.5}}>
+                            <Stack spacing={1}>
+                                <Button onClick={() => setEditAddressBtn(false)} sx={{border: 'solid 1px red', px: 0}}>
+                                    <CloseIcon sx={{color: 'red'}}/>
+                                </Button>
+                                <Button sx={{border: 'solid 1px green'}}>
+                                    <CheckIcon sx={{color: 'green'}}/>
+                                </Button>
+                            </Stack>
+                        </Box>
+                    </>                     
+
+                    :
+                    <>
+                        <Typography sx={{display: 'inline-flex', mx: 2.5}}>{user.address}</Typography>
+                        <Button onClick={() => setEditAddressBtn(true)} variant='outlined' sx={{display: 'inline-flex', mr: 1.5}}>Edit</Button>
+                    </>
+                    }
                 </Card>
             </Box>
         </>
