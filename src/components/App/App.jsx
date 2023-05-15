00f486da12c761b67b347b617249f6bad1b3d229
Siv Earley <searley96@gmail.com>
2023-05-15 11:34:25 -0500

    added client folder and form files

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -19,6 +19,7 @@ import InfoPage from '../InfoPage/InfoPage';
 import LandingPage from '../LandingPage/LandingPage';
 import LoginPage from '../LoginPage/LoginPage';
 import RegisterPage from '../RegisterPage/RegisterPage';
+import KitchenForm from '../Client/KitchenForm';
 
 import './App.css';
 
---
+++ src/components/Client/KitchenForm.jsx
@@ -0,0 +1,11 @@
+function KitchenForm() {
+return (
+    <>
+    <h1>Bathrooms</h1>
+    <h2>How many bathrooms?</h2>
+    <h3>Please check one.</h3>
+    </>
+)
+}
+
+export default KitchenForm;
