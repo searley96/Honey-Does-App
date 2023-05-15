02dee56fae5d9fb3ade696eeca326c0842cf3258
Fue Lee <fueclee.12@gmail.com>
2023-05-15 14:30:22 -0500

    styled profile page to reflect wireframe

--- src/components/ProfilePage/ProfilePage.jsx
+++ src/components/ProfilePage/ProfilePage.jsx
@@ -1,32 +1,56 @@
-import { AppBar, Box, Button, Toolbar, CssBaseline, Fab, IconButton, Menu, MenuItem, Typography } from '@mui/material';
+import { AppBar, Box, Button, Toolbar, Card, CssBaseline, Fab, IconButton, Menu, MenuItem, Typography } from '@mui/material';
 
 
 function ProfilePage () {
 
+    const cardStyle = {
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        width: '90%',
+        paddingY: 1,
+        my: 1
+    }
+
     return(
         <>
-            <h1>This is the Client Profile Page</h1>
-            <Typography align='center'>
+            <Typography align='center' variant='h5' sx={{mt: 3, mb: 1}}>
                 My Information
             </Typography>
-            <Box sx={{}}>
-                <Typography align='center'>
+            <Box sx={{textAlign: 'center',
+                      display: 'flex',
+                      flexDirection: 'column',
+                      alignItems: 'center'
+                    }}>
+                <Typography align='center' variant='h6'>
                     Email:
                 </Typography>
-                <Button>Edit</Button>
+                <Card sx={cardStyle}>
+                    <Typography sx={{display: 'inline-flex', ml: 2}}>User Email Text</Typography>
+                    <Button variant='outlined' sx={{display: 'inline-flex', mr: 1}}>Edit</Button>
+                </Card>
+                <Typography align='center' variant='h6'>
+                    Password:
+                </Typography>
+                <Card sx={cardStyle}>
+                    <Typography sx={{display: 'inline-flex', ml: 2}}>User Password Text</Typography>
+                    <Button variant='outlined' sx={{display: 'inline-flex', mr: 1}}>Edit</Button>
+                </Card>
+                <Typography align='center' variant='h6'>
+                    Phone Number:
+                </Typography>
+                <Card sx={cardStyle}>
+                    <Typography sx={{display: 'inline-flex', ml: 2}}>User Phone Number Text</Typography>
+                    <Button variant='outlined' sx={{display: 'inline-flex', mr: 1}}>Edit</Button>
+                </Card>
+                <Typography align='center' variant='h6'>
+                    Address:
+                </Typography>
+                <Card sx={cardStyle}>
+                    <Typography sx={{display: 'inline-flex', ml: 2}}>User Address Text</Typography>
+                    <Button variant='outlined' sx={{display: 'inline-flex', mr: 1}}>Edit</Button>
+                </Card>
             </Box>
-            <Typography align='center'>
-                Password:
-            </Typography>
-            <Button>Edit</Button>
-            <Typography align='center'>
-                Phone Number:
-            </Typography>
-            <Button>Edit</Button>
-            <Typography align='center'>
-                Address:
-            </Typography>
-            <Button>Edit</Button>
         </>
     )
 
