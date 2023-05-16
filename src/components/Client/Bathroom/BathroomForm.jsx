13a26641a8a2e86d508853bd14215af35218931d
Siv Earley <searley96@gmail.com>
2023-05-16 16:06:46 -0500

    completed radio buttons, checkboxes and input fields on bathroom form

--- src/components/Client/Bathroom/BathroomForm.jsx
+++ src/components/Client/Bathroom/BathroomForm.jsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import { useState } from "react";
+import "./BathroomForm.css";
 
 import {
   FormLabel,
@@ -9,38 +10,63 @@ import {
   Radio,
   Checkbox,
   FormGroup,
-  //   InputLabel,
-  //   Select,
-  //   MenuItem,
-  //   Box,
 } from "@mui/material";
-
-const label = { inputProps: { "aria-label": "Checkbox demo" } };
 
 function BathroomForm() {
   const label = { inputProps: { "aria-label": "Checkbox demo" } };
 
+  //conditional rendering for input box when 'other' is checked on bathtub type section
+  const handleBathtubCheckboxChange = (event) => {
+    setIsOtherChecked(event.target.checked);
+  };
+
+  //collect text input value for 'other'
+  const handleBathtubInputChange = (event) => {
+    setOtherInputValue(event.target.value);
+  };
+  console.log("other tub info:", event.target.value);
+  const [isOtherChecked, setIsOtherChecked] = useState(false);
+  const [otherInputValue, setOtherInputValue] = useState("");
+
+  //conditional rendering to show number of mirrors when 'yes' is clicked on clean mirror
+  const [mirror, setMirror] = useState("yes");
+  const [showMirrorCount, setMirrorCount] = useState(false);
+
+  //conditional rendering to show more questions when 'cabinets underneath' is selected for sink type
   const [sinkType, setSinkType] = useState("pedistal");
   const [showCabinetsOption, setShowCabinetsOption] = useState(false);
 
+  //condtional rendering to show more questions when 'yes' selected for cabinet clean
   const [cleanCabinets, setCleanCabinets] = useState("yes");
-  const [showCleanCabinets, setShowCleanCabinets] = useState(false)
+  const [showCleanCabinets, setShowCleanCabinets] = useState(false);
 
+  //conditional rendering to show rug question when 'yes' is selected for sweep and mop floors
   const [cleanFloors, setCleanFloors] = useState("yes");
-  const [showCleanRugs, setShowCleanRugs] = useState(false)
+  const [showCleanRugs, setShowCleanRugs] = useState(false);
+
+  //conditional rendering for input box when 'yes' is selected on trash section
+  const handleTrashCheckboxChange = (event) => {
+    setIsYesChecked(event.target.checked);
+  };
+
+  //collect text input value for trash instructions
+  const handleTrashInputChange = (event) => {
+    setTrashInputValue(event.target.value);
+  };
+  console.log("trash instructions:", event.target.value);
+
+  const [isYesChecked, setIsYesChecked] = useState(false);
+  const [trashInputValue, setTrashInputValue] = useState("");
 
   return (
     <>
       <h1>Bathroom</h1>
 
       <FormControl>
-        <FormLabel id="bathroom-type-buttons-group-label">
-          Bathroom Type
-        </FormLabel>
+        <FormLabel id="bathroom-type">Bathroom Type</FormLabel>
         <RadioGroup
           row
-          aria-labelledby="bathroom-type-buttons-group-label"
-          defaultValue="full bath"
+          aria-labelledby="bathroom-type"
           name="bathroom-type-group"
         >
           <FormControlLabel
@@ -59,91 +85,90 @@ function BathroomForm() {
             label="Don't Clean"
           />
         </RadioGroup>
-      </FormControl>
 
-      <FormGroup>
-        <FormLabel id="bathroom-type-buttons-group-label">
-          Does this bathroom have any of the following? <br />
-          Check all that apply.
-        </FormLabel>
-        <FormControlLabel
-          control={<Checkbox defaultChecked />}
-          label="Regular plastic tub/shower combo"
-        />
-        <FormControlLabel
-          control={<Checkbox />}
-          label="Ceramic or porcelain tub"
-        />
-        <FormControlLabel control={<Checkbox />} label="Walk-in shower" />
-        <FormControlLabel
-          control={<Checkbox />}
-          label="Specialty walk-in shower"
-        />
-        <FormControlLabel control={<Checkbox />} label="Jacuzzi tub" />
-        <FormControlLabel
-          control={<Checkbox />}
-          label="Other - please specify"
-        />
-      </FormGroup>
+        <FormGroup>
+          <FormLabel id="bathroom-tub-type">
+            Does this bathroom have any of the following? <br />
+            Check all that apply.
+          </FormLabel>
+          <FormControlLabel
+            control={<Checkbox />}
+            label="Regular plastic tub/shower combo"
+          />
+          <FormControlLabel
+            control={<Checkbox />}
+            label="Ceramic or porcelain tub"
+          />
+          <FormControlLabel control={<Checkbox />} label="Walk-in shower" />
+          <FormControlLabel
+            control={<Checkbox />}
+            label="Specialty walk-in shower"
+          />
+          <FormControlLabel control={<Checkbox />} label="Jacuzzi tub" />
+          <FormControlLabel
+            control={<Checkbox onChange={handleBathtubCheckboxChange} />}
+            label="Other"
+          />
+          {isOtherChecked && (
+            <input
+              type="text"
+              placeholder="Please specify."
+              value={otherInputValue}
+              onChange={handleBathtubInputChange}
+            />
+          )}
+        </FormGroup>
 
-      <FormControl>
-        <FormLabel id="mirror-type-buttons-group-label">
-          Clean Mirror?
-        </FormLabel>
+        <FormLabel id="mirror-type">Clean Mirror?</FormLabel>
         <RadioGroup
           row
-          aria-labelledby="mirror-type-buttons-group-label"
-          defaultValue="yes"
+          aria-labelledby="mirror-type"
           name="mirror-type-group"
+          onChange={(event) => {
+            setMirror(event.target.value);
+            setMirrorCount(event.target.value === "yes");
+          }}
         >
           <FormControlLabel value="yes" control={<Radio />} label="Yes" />
           <FormControlLabel value="no" control={<Radio />} label="No" />
         </RadioGroup>
-        <FormLabel id="mirror-type-buttons-group-label">
-          How many mirrors?
-        </FormLabel>
-        <RadioGroup
-          row
-          aria-labelledby="mirror-count-buttons-group-label"
-          defaultValue="1"
-          name="mirror-count-group"
-        >
-          <FormControlLabel value="1" control={<Radio />} label="1" />
-          <FormControlLabel value="2" control={<Radio />} label="2" />
-          <FormControlLabel value="3" control={<Radio />} label="3" />
-          <FormControlLabel value="4" control={<Radio />} label="4" />
-        </RadioGroup>
-      </FormControl>
-      <FormControl>
-        <FormLabel id="sink-clean-buttons-group-label">
-          Clean sinks/counters?
-        </FormLabel>
-        <RadioGroup
-          row
-          aria-labelledby="sink-clean-buttons-group-label"
-          defaultValue="yes"
-          name="sink-clean-group"
-        >
+        {showMirrorCount && (
+          <>
+            <FormLabel id="mirror-count">How many mirrors?</FormLabel>
+            <RadioGroup
+              row
+              aria-labelledby="mirror-count"
+              defaultValue="1"
+              name="mirror-count-group"
+            >
+              <FormControlLabel value="1" control={<Radio />} label="1" />
+              <FormControlLabel value="2" control={<Radio />} label="2" />
+              <FormControlLabel value="3" control={<Radio />} label="3" />
+              <FormControlLabel value="4" control={<Radio />} label="4" />
+            </RadioGroup>
+          </>
+        )}
+
+        <FormLabel id="sink-clean">Clean sinks/counters?</FormLabel>
+        <RadioGroup row aria-labelledby="sink-clean" name="sink-clean-group">
           <FormControlLabel value="yes" control={<Radio />} label="Yes" />
           <FormControlLabel value="no" control={<Radio />} label="No" />
         </RadioGroup>
-        <FormLabel id="sink-clean-buttons-group-label">
+        <FormLabel id="granite-tops">
           Do you have granite counter tops?
         </FormLabel>
         <RadioGroup
           row
-          aria-labelledby="sink-clean-buttons-group-label"
-          defaultValue="yes"
-          name="sink-clean-group"
+          aria-labelledby="granite-tops"
+          name="granite-tops-group"
         >
           <FormControlLabel value="yes" control={<Radio />} label="Yes" />
           <FormControlLabel value="no" control={<Radio />} label="No" />
         </RadioGroup>
-        <FormLabel id="sink-type-buttons-group-label">Sink type?</FormLabel>
+        <FormLabel id="sink-type">Sink type?</FormLabel>
         <RadioGroup
           row
-          aria-labelledby="sink-type-buttons-group-label"
-          defaultValue="pedistal"
+          aria-labelledby="sink-type"
           name="sink-type-group"
           onChange={(event) => {
             setSinkType(event.target.value);
@@ -163,13 +188,12 @@ function BathroomForm() {
         </RadioGroup>
         {showCabinetsOption && (
           <>
-            <FormLabel id="cab-clean-buttons-group-label">
+            <FormLabel id="cab-clean">
               If it has cabinets, do you want the fronts cleaned?
             </FormLabel>
             <RadioGroup
               row
-              aria-labelledby="cab-clean-buttons-group-label"
-              defaultValue="no"
+              aria-labelledby="cab-clean"
               name="cab-clean-group"
               onChange={(event) => {
                 setCleanCabinets(event.target.value);
@@ -183,128 +207,102 @@ function BathroomForm() {
         )}
         {showCleanCabinets && (
           <>
-            <FormLabel id="cab-clean-type-buttons-group-label">
+            <FormLabel id="cab-clean-type">
               If yes, how do you want them cleaned?
             </FormLabel>
             <RadioGroup
               row
-              aria-labelledby="cab-clean-type-buttons-group-label"
-              defaultValue="spot"
+              aria-labelledby="cab-clean-type"
               name="cab-clean-type-group"
             >
-              <FormControlLabel value="spot" control={<Radio />} label="Spot clean" />
-              <FormControlLabel value="full" control={<Radio />} label="Full wipe down" />
-              <FormLabel id="polish-cab-buttons-group-label">
-              If wood cabinets, do you want us to use Orange Glo polish?
-            </FormLabel>
-            <RadioGroup
-              row
-              aria-labelledby="polish-cab-buttons-group-label"
-              defaultValue="yes"
-              name="polish-cab-group"
-            >
-              <FormControlLabel value="yes" control={<Radio />} label="Yes" />
-              <FormControlLabel value="no" control={<Radio />} label="No" />
-            </RadioGroup>
+              <FormControlLabel
+                value="spot"
+                control={<Radio />}
+                label="Spot clean"
+              />
+              <FormControlLabel
+                value="full"
+                control={<Radio />}
+                label="Full wipe down"
+              />
+              <FormLabel id="polish-cab">
+                If wood cabinets, do you want us to use Orange Glo polish?
+              </FormLabel>
+              <RadioGroup
+                row
+                aria-labelledby="polish-cab"
+                name="polish-cab-group"
+              >
+                <FormControlLabel value="yes" control={<Radio />} label="Yes" />
+                <FormControlLabel value="no" control={<Radio />} label="No" />
+              </RadioGroup>
             </RadioGroup>
           </>
         )}
-      </FormControl>
-      <FormControl>
-        <FormLabel id="toilet-type-buttons-group-label">
+
+        <FormLabel id="toilet-type">
           Do you want us to clean the toilet?
         </FormLabel>
-        <RadioGroup
-          row
-          aria-labelledby="toilet-type-buttons-group-label"
-          defaultValue="yes"
-          name="toilet-type-group"
-        >
-          <FormControlLabel
-            value="yes"
-            control={<Radio />}
-            label="Yes"
-          />
-          <FormControlLabel
-            value="no"
-            control={<Radio />}
-            label="No"
-          />
+        <RadioGroup row aria-labelledby="toilet-type" name="toilet-type-group">
+          <FormControlLabel value="yes" control={<Radio />} label="Yes" />
+          <FormControlLabel value="no" control={<Radio />} label="No" />
         </RadioGroup>
-      </FormControl>
-      <FormControl>
-        <FormLabel id="trash-buttons-group-label">
-          Do you want us to take out the trash?
-        </FormLabel>
-        <RadioGroup
-          row
-          aria-labelledby="trash-buttons-group-label"
-          defaultValue="yes"
-          name="trash-group"
-        >
+
+        <FormLabel id="trash">Do you want us to take out the trash?</FormLabel>
+        <RadioGroup row aria-labelledby="trash" name="trash-group">
           <FormControlLabel
-            value="yes"
-            control={<Radio />}
+            control={<Radio onChange={handleTrashCheckboxChange} />}
             label="Yes"
+            value="yes"
           />
           <FormControlLabel
-            value="no"
-            control={<Radio />}
+            control={<Radio  />}
             label="No"
+            value="no"
           />
+
+          {isYesChecked && (
+            <input
+              type="text"
+              placeholder="Removal instructions."
+              value={trashInputValue}
+              onChange={handleTrashInputChange}
+            />
+          )}
         </RadioGroup>
-      </FormControl> 
-      <FormControl>
-        <FormLabel id="floors-buttons-group-label">
+
+        <FormLabel id="floors">
           Do you want us to sweep and mop the floors?
         </FormLabel>
         <RadioGroup
           row
-          aria-labelledby="floors-buttons-group-label"
-          defaultValue="no"
+          aria-labelledby="floors"
           name="floors-group"
           onChange={(event) => {
             setCleanFloors(event.target.value);
             setShowCleanRugs(event.target.value === "yes");
           }}
         >
-          <FormControlLabel
-            value="yes"
-            control={<Radio />}
-            label="Yes"
-          />
-          <FormControlLabel
-            value="no"
-            control={<Radio />}
-            label="No"
-          />
+          <FormControlLabel value="yes" control={<Radio />} label="Yes" />
+          <FormControlLabel value="no" control={<Radio />} label="No" />
         </RadioGroup>
         {showCleanRugs && (
           <>
-        <FormLabel id="rugs-buttons-group-label">
-          If so, do you have accent rugs that need to be moved/shaken off beforehand?
-        </FormLabel>
-        <RadioGroup
-          row
-          aria-labelledby="rugs-buttons-group-label"
-          defaultValue="yes"
-          name="rugs-group"
-        >
-          <FormControlLabel
-            value="yes"
-            control={<Radio />}
-            label="Yes"
-          />
-          <FormControlLabel
-            value="no"
-            control={<Radio />}
-            label="No"
-          />
-        </RadioGroup>
-        </>
+            <FormLabel id="floors-rugs">
+              Do you have accent rugs that need to be moved/shaken off
+              beforehand?
+            </FormLabel>
+            <RadioGroup
+              row
+              aria-labelledby="floors-rugs"
+              name="floors-rugs-group"
+            >
+              <FormControlLabel value="yes" control={<Radio />} label="Yes" />
+              <FormControlLabel value="no" control={<Radio />} label="No" />
+            </RadioGroup>
+          </>
         )}
-      </FormControl> 
-
+      </FormControl>
     </>
   );
 }
