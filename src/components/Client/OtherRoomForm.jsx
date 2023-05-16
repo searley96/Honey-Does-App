d29f1745bbc8801e572db7e8b90549490aecd083
Ia Vue <iavue00@gmail.com>
2023-05-16 15:57:04 -0500

    made progress to create form inside the return

--- src/components/Client/OtherRoomForm.jsx
+++ src/components/Client/OtherRoomForm.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { useHistory } from 'react-router-dom';
-import { TextField, FormControl, FormGroup, RadioGroup, Checkbox, FormLabel, FormControlLabel, Radio } from '@mui/material';
+import { TextField, FormControl, FormGroup, RadioGroup, Checkbox, FormLabel, FormControlLabel, InputLabel, Input, Radio } from '@mui/material';
 
 function OtherRoomForm() {
     console.log('Inside OtherRoomForm()')
@@ -18,7 +18,7 @@ function OtherRoomForm() {
 
     // QUESTION: What if the client does not want other rooms cleaned? What do we send to the db?
     // capture user inputs and set in to "otherRoomForm"
-    const setOtherRoomForm = (event) => {
+    const setForm = (event) => {
         let targetInputField = event.target.id;
         switch (targetInputField) {
             case '1':
@@ -53,7 +53,58 @@ function OtherRoomForm() {
     return (
         <FormGroup>
             <FormControl>
-                <FormLabel></FormLabel>
+                {/* What if the client has multiple other rooms that needs a cleaning? For ex: bedroom, office, living room, etc. 
+                How do we capture that to store in the db? So for now, I'm building the form so that it's only taking one room. */}
+                <FormLabel>Would you like to request a cleaning for another room? If so, select the type of room.</FormLabel>
+                <RadioGroup aria-labelledby="room_type" name="room_type"
+                    row defaultValue="yes" value={otherRoomForm.room_type}
+                    onChange={setForm}>
+                    <FormControlLabel value="bedroom" control={<Radio />} label="Bedroom" />
+                    <FormControlLabel value="livingRoom" control={<Radio />} label="Living Room" />
+                    <FormControlLabel value="diningRoom" control={<Radio />} label="Dining Room" />
+                    <FormControlLabel value="office" control={<Radio />} label="Office" />
+                    <FormControlLabel value="laundry" control={<Radio />} label="Laundry" />
+                    <FormControlLabel value="other" control={<Radio />} label="Other" />
+                </RadioGroup>
+                {/* Maybe have a textbox here for the client to type in the type of room that's not listed here */}
+
+                {/* The input isn't in the place I want it to be. */}
+                <InputLabel htmlFor="sq_ft">What is the square footage of the room?</InputLabel>
+                <Input id="sq_ft" />
+
+
+                <FormLabel>Do surfaces need to be wiped?</FormLabel>
+                <RadioGroup aria-labelledby="wipe_surfaces" name="wipe_surfaces"
+                    row defaultValue="yes" value={otherRoomForm.wipe_surfaces}
+                    onChange={setForm}>
+                    <FormControlLabel value="yes" control={<Radio />} label="Yes" />
+                    <FormControlLabel value="no" control={<Radio />} label="No" />
+                </RadioGroup>
+
+                <FormLabel>Do floors need to be cleaned?</FormLabel>
+                <RadioGroup aria-labelledby="clean_floor" name="clean_floor"
+                    row defaultValue="yes" value={otherRoomForm.clean_floor}
+                    onChange={setForm}>
+                    <FormControlLabel value="yes" control={<Radio />} label="Yes" />
+                    <FormControlLabel value="no" control={<Radio />} label="No" />
+                </RadioGroup>
+
+                <FormLabel>If floors need to be cleaned, please select floor type.</FormLabel>
+                <RadioGroup aria-labelledby="floor_type" name="floor_type"
+                    row defaultValue="yes" value={otherRoomForm.floor_type}
+                    onChange={setForm}>
+                    <FormControlLabel value="wood" control={<Radio />} label="Wood" />
+                    <FormControlLabel value="carpet" control={<Radio />} label="Carpet" />
+                    <FormControlLabel value="tiles" control={<Radio />} label="Tiles" />
+                    <FormControlLabel value="laminate" control={<Radio />} label="Laminate" />
+                    <FormControlLabel value="vinyl" control={<Radio />} label="Vinyl" />
+                    <FormControlLabel value="other" control={<Radio />} label="Other" />
+                    <FormControlLabel value="na" control={<Radio />} label="N/A" />
+                </RadioGroup>
+                {/* Maybe have a textbox here for the client to type in the type of floor that's not listed here */}
+
+
+
             </FormControl>
         </FormGroup>
     )
