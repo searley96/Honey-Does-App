f5504b17ff48fd7c0fa664b0e8d65deddb9b0540
Siv Earley <searley96@gmail.com>
2023-05-17 13:27:10 -0500

    created BathroomType component

--- src/components/Client/Bathroom/BathroomType.jsx
+++ src/components/Client/Bathroom/BathroomType.jsx
@@ -1,46 +1,60 @@
-import React from 'react';
-import { useDispatch } from 'react-redux';
-import { useSelector } from 'react-redux';
-import {  FormControl, FormGroup, RadioGroup, FormLabel, FormControlLabel, Radio } from '@mui/material';
+import React from "react";
+import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
+import {
+  FormControl,
+  FormGroup,
+  RadioGroup,
+  FormLabel,
+  FormControlLabel,
+  Radio,
+} from "@mui/material";
 
 function BathroomType() {
+  const bathroom = useSelector((store) => store.clientBathroomReducer);
+  const dispatch = useDispatch();
+//   console.log('bathroom', bathroom)
 
-    const bathroom = useSelector(store => store.clientBathroomReducer)
-    const dispatch = useDispatch();
-
-    return (
-        <>
-        <h3>Bathroom Type</h3>
-        <FormGroup>
-            <FormControl>
-
-            <FormLabel id="bathroom-type">Bathroom Type</FormLabel>
-        <RadioGroup
-          aria-labelledby="bathroom-type"
-          name="bathroom-type-group"
-          row value={bathroom.room_type}
-         
-        >
-          <FormControlLabel
-            value="full-bath"
-            control={<Radio />}
-            label="Full Bath"
-          />
-          <FormControlLabel
-            value="half-bath"
-            control={<Radio />}
-            label="Half Bath"
-          />
-          <FormControlLabel
-            value="no-bath"
-            control={<Radio />}
-            label="Don't Clean"
-          />
-        </RadioGroup>
-            </FormControl>
-        </FormGroup>
-        </>
-    )
+  return (
+    <>
+      <h3>Bathroom Type</h3>
+      <FormGroup>
+        <FormControl>
+          <FormLabel id="bathroom-type">Bathroom Type</FormLabel>
+          <RadioGroup
+            aria-labelledby="bathroom-type-group"
+            name="bathroom-type-group"
+            row
+            value={bathroom.bathroom_type}
+            onChange={event =>
+               
+              dispatch({
+                type: "SET_BATHROOM_TYPE",
+                payload: event.target.value,
+        
+              })
+            }
+          >
+            <FormControlLabel
+              value="Full Bath"
+              control={<Radio />}
+              label="Full Bath"
+            />
+            <FormControlLabel
+              value="Half Bath"
+              control={<Radio />}
+              label="Half Bath" aria-checked={true}
+            />
+            <FormControlLabel
+              value="Don't Clean"
+              control={<Radio />}
+              label="Don't Clean"
+            />
+          </RadioGroup>
+        </FormControl>
+      </FormGroup>
+    </>
+  );
 }
 
 export default BathroomType;
