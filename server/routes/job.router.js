b27d27cdc5188a5f7a0254921cf18378ad629b1b
Fue Lee <fueclee.12@gmail.com>
2023-05-17 14:07:40 -0500

    created job.saga to handle jobID transition from button click to server to reducer

--- server/routes/job.router.js
+++ server/routes/job.router.js
@@ -3,9 +3,12 @@ const pool = require('../modules/pool');
 const router = express.Router();
 
 
+// ********** Job ID Generator **********
+router.get('/jobid', (req, res) => {
     // jobID generator
-        // this is a function to generate a random 6 character string of numbers to the pass to the server
-            // along with new request information
+
+    // this is a function to generate a random 6 character string of numbers to the pass to the server
+        // along with new request information
     function jobIDGenerator (min, max) {
         let numberString = '';
         for(let i = 0; i < 6; i++) {
@@ -14,10 +17,14 @@ const router = express.Router();
         }
         return numberString;
     }
+    // this function checks a number against another number
+        // the main purpose is to check the randomly generated number against the array of jobIDs pulled
+        // from the database, if a duplicate is found, the function runs again, generating a new random
+        // number string, checking against the jobIDs pulled from the database again, until a viable jobID is found
     const checkRandomNumberDupes = (number1, number2, array1, array2) => {
-        console.log('inside checkRandomNumberDupes');
-        console.log('this is number1', number1);
-        console.log('this is number2', number2);
+        // console.log('inside checkRandomNumberDupes');
+        // console.log('this is number1', number1);
+        // console.log('this is number2', number2);
         if(number1 === number2) {
             console.log('duplicate found, creating new number');
             checkRandomNumberDupes(jobIDGenerator(0, 9), number2, array1, array2);
@@ -27,25 +34,29 @@ const router = express.Router();
             }
         }
     }
+    // pulls jobIDs from the database
     const queryJobID = `SELECT "job_id" FROM "job"`
     pool.query(queryJobID)
     .then((result) => {
-        console.log(result.rows);
+        // console.log(result.rows);
         const jobIdArray = [];
         const approvedJobIdArray = [];
         const jobId = jobIDGenerator(0, 9);
         result.rows.forEach(id => {
             jobIdArray.push(id.job_id);
         })
-        console.log(jobIdArray);
+        // console.log(jobIdArray);
         jobIdArray.forEach(id => {
             checkRandomNumberDupes(jobId, id, jobIdArray, approvedJobIdArray);
         });
-        console.log(approvedJobIdArray);
+        // console.log(approvedJobIdArray);
         const approvedJobID = approvedJobIdArray[0];
-        console.log(approvedJobID);
+        // console.log(approvedJobID);
+        res.send(approvedJobID);
     }).catch(() => {
         console.log('ERROR getting Job ID list');
     })
+});
+// END ********** Job ID Generator **********
 
 module.exports = router;
