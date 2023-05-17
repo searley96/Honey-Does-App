49b062aaf18c0bd73bb8f13f7405a86d0641efe2
James Jacobsen <edjcjacobsen@gmail.com>
2023-05-16 20:33:14 -0500

    moved MicrowaveGroup to its own component jsx file

--- src/components/Client/Kitchen/KitchenForm.jsx
+++ src/components/Client/Kitchen/KitchenForm.jsx
@@ -1,15 +1,9 @@
-import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { useSelector } from 'react-redux';
-import { TextField, FormControl, FormGroup, RadioGroup, Checkbox, FormLabel, FormControlLabel, Radio } from '@mui/material';
 import CabinetGroup from './CabinetGroup';
 import StoveGroup from './StoveGroup';
 import ApplianceGroup from './ApplianceGroup';
+import MicrowaveGroup from './MicrowaveGroup';
 
 function KitchenForm() {
-    
-    const kitchen = useSelector(store => store.clientKitchen)
-    const dispatch = useDispatch();
 
     return (
 
@@ -24,26 +18,7 @@ function KitchenForm() {
     );
 
    
-    function MicrowaveGroup() {
-
-        return (
-            <>
-                <h3>Microwave</h3>
-                <FormGroup>
-                    <FormControl>
-                        {/* CLEAN MICROWAVE */}
-                        <FormLabel>Do you want us to clean your microwave?</FormLabel>
-                        <RadioGroup aria-labelledby="clean_microwave" name="clean_microwave"
-                            row value={kitchen.clean_microwave}
-                            onChange={e => dispatch({ type: 'SET_MICROWAVE', payload: event.target.value })}>
-                            <FormControlLabel value="yes" control={<Radio />} label="yes" />
-                            <FormControlLabel value="no" control={<Radio />} label="no" />
-                        </RadioGroup>
-                    </FormControl>
-                </FormGroup>
-            </>
-        )
-    }  
+     
 }
 
 export default KitchenForm;
