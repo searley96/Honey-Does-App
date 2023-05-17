5436bfacca4fcb455b5a48d30960ab58e9a2ecf8
James Jacobsen <edjcjacobsen@gmail.com>
2023-05-17 15:39:06 -0500

    added margin to kitchen form groups

--- src/components/Client/Kitchen/KitchenForm.jsx
+++ src/components/Client/Kitchen/KitchenForm.jsx
@@ -4,9 +4,10 @@ import ApplianceGroup from './ApplianceGroup';
 import MicrowaveGroup from './MicrowaveGroup';
 import CountersSinksGroup from './CountersSinksGroup';
 import KitchenFloors from './KitchenFloors';
-import { Button, ButtonGroup } from '@mui/material';
+import { Button, ButtonGroup, Stack, Box } from '@mui/material';
 import AddIcon from '@mui/icons-material/Add';
 import DoneIcon from '@mui/icons-material/Done';
+
 import './Kitchen.css';
 
 function KitchenForm() {
@@ -14,16 +15,28 @@ function KitchenForm() {
     return (
 
         <>
-            <CabinetGroup className='formGroup'/>
-            <ApplianceGroup className='formGroup'/>
-            <MicrowaveGroup className='formGroup'/>
-            <StoveGroup className='formGroup'/>
-            <CountersSinksGroup className='formGroup'/>
-            <KitchenFloors />
-            <ButtonGroup sx={{mt: '40px', width: '100%'}}>
-                <Button variant="outlined"><AddIcon sx={{ mx: '5px'}} fontSize="small" /> Add Another Kitchen</Button>
-                <Button variant="contained"><DoneIcon sx={{ mx: '5px'}} fontSize="small" /> Done with kitchen forms</Button>
-            </ButtonGroup>
+            <Box sx={{ my: '60px' }}>
+                <CabinetGroup className='form-group' />
+            </Box>
+            <Box sx={{ my: '60px' }}>
+                <ApplianceGroup className='form-group' />
+            </Box>
+            <Box sx={{ my: '60px' }}>
+                <MicrowaveGroup className='form-group' />
+            </Box>
+            <Box sx={{ my: '60px' }}>
+                <StoveGroup className='form-group' />
+            </Box>
+            <Box sx={{ my: '60px' }}>
+                <CountersSinksGroup className='form-group' />
+            </Box>
+            <Box sx={{ my: '60px' }}>
+                <KitchenFloors />
+            </Box>
+            <Stack spacing={1} direction='row' sx={{ mt: '40px', display: 'flex', justifyContent: 'space-between' }}>
+                <Button variant="outlined"><AddIcon sx={{ mx: '5px' }} fontSize="small" /> Add Another Kitchen</Button>
+                <Button variant="contained"><DoneIcon sx={{ mx: '5px' }} fontSize="small" /> Done with kitchen forms</Button>
+            </Stack>
 
         </>
     );
