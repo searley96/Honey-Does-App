b8d6ef65182719397cd9b8a804a9145324a6bfe8
Fue Lee <fueclee.12@gmail.com>
2023-05-18 10:03:28 -0500

    jobid reducer functions

--- src/redux/reducers/_root.reducer.js
+++ src/redux/reducers/_root.reducer.js
@@ -2,11 +2,14 @@ import { combineReducers } from 'redux';
 import errors from './errors.reducer';
 import user from './user.reducer';
 import clientKitchen from './clientKitchen.reducer';
+import jobidReducer from './jobid.reducer';
 
 const rootReducer = combineReducers({
   errors, // contains registrationMessage and loginMessage
   user,
-  clientKitchen // will have an id and username if someone is logged in
+  clientKitchen, // will have an id and username if someone is logged in
+  jobidReducer
+  
 });
 
 export default rootReducer;
