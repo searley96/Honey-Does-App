1af896ceabb7d408f0d78e01e1ea0d4c7a9283e5
Siv Earley <searley96@gmail.com>
2023-05-25 15:48:09 -0500

    added styling to the adminDashboard component

--- src/components/Admin/AdminDashboard/AdminDashboard.jsx
+++ src/components/Admin/AdminDashboard/AdminDashboard.jsx
@@ -1,53 +1,74 @@
 import React, { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { Button, Card, CardActions, CardContent, CardMedia, Typography, Link } from '@mui/material';
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Typography,
+  Link,
+} from "@mui/material";
 import { useHistory } from "react-router-dom";
 import JobDetails from "./JobDetails";
 
 function AdminDashboard() {
-    console.log('Inside Dashboard()!!');
-    const dispatch = useDispatch();
+  console.log("Inside Dashboard()!!");
+  const dispatch = useDispatch();
 
-    const allJobs = useSelector(store => store.allJobs);
-    console.log('All jobs:', allJobs);
+  const allJobs = useSelector((store) => store.allJobs);
+  console.log("All jobs:", allJobs);
 
-    useEffect(() => {
-        dispatch({ type: "FETCH_JOBS" }); // Fetch job ID
-    }, []);
+  useEffect(() => {
+    dispatch({ type: "FETCH_JOBS" }); // Fetch job ID
+  }, []);
 
-    const history = useHistory();
+  const history = useHistory();
 
-    const handleOnClick = (jobObj) => {
-        dispatch({ type: 'VIEW_JOB_DETAILS', payload: jobObj})
-        history.push('/jobDetails')
-    }
+  const handleOnClick = (jobObj) => {
+    dispatch({ type: "VIEW_JOB_DETAILS", payload: jobObj });
+    history.push("/jobDetails");
+  };
 
-    return (
-        <>
-            <h3>All Jobs</h3>
-            {allJobs.allJobs.length ? (
-                allJobs.allJobs.map((job, i) => (
-                    <Card key={i}>
-                        <CardContent sx={{ display: "flex", justifyContent: "space-between" }}>
-                            <Button style={{ display: 'inline-block' }} variant='outlined' onClick={() => handleOnClick(job)}>
-                                {job.job_id}
-                            </Button>
-                            <Typography style={{ display: 'inline-block'}}>
-                                {job.client_first_name} {job.client_last_name}
-                            </Typography>
-                            <Typography style={{ display: 'inline-block' }}>
-                                {job.job_status}
-                            </Typography>
-                        </CardContent>
-                    </Card>
-                ))
-            ) : (
-                <p>No details yet.</p>
+  return (
+    <>
+      <Typography
+        sx={{ fontWeight: "bold", mt: 3 }}
+        align="center"
+        variant="h5"
+        
+      >
+        All Jobs
+      </Typography>
+      <Card elevation="0">
+        <CardContent sx={{ display: "flex", justifyContent: "space-between"}}>
+          <Typography sx={{ marginBottom: -3, marginLeft: 3 }}>Job ID:</Typography>
+          <Typography sx={{ marginBottom: -3 }}>Client Name:</Typography>
+          <Typography sx={{ marginBottom: -3 , marginRight:2 }}>Status:</Typography>
+        </CardContent>
+      </Card>
 
-            )}
-
-        </>
-    )
+      {allJobs.allJobs.length ? (
+  allJobs.allJobs.map((job, i) => (
+    <Card key={i} sx={{ mt: 2, pt: 1, marginLeft: 2, marginRight: 2, backgroundColor: "whitesmoke" }}>
+      <CardContent sx={{ display: "flex", alignItems: "center", justifyContent: "space-between" }}>
+        <Button sx={{ marginLeft: -1}}variant="outlined" onClick={() => handleOnClick(job)}>
+          {job.job_id}
+        </Button>
+        <Typography sx={{ fontWeight: "bold", flex: 1, marginLeft: 6 }}>
+          {job.client_first_name} {job.client_last_name}
+        </Typography>
+        <Typography sx={{  marginRight: .8 }}>
+          {job.job_status}
+        </Typography>
+      </CardContent>
+    </Card>
+        ))
+      ) : (
+        <p>No details yet.</p>
+      )}
+    </>
+  );
 }
 
 export default AdminDashboard;
